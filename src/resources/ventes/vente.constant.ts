73dd2e2bec29a5ee7a680a4424dabb573f902680
kdiawara96 <karimdiawara96@gmail.com>
2024-09-20 12:29:53 +0000

    erreur ok

--- src/resources/ventes/vente.constant.ts
+++ src/resources/ventes/vente.constant.ts
@@ -12,7 +12,7 @@ const errors = {
     NOT_REMOVABLE: 'Vous ne pouvez pas supprimer cette vente car elle peut être liée à un article\nVous pouvez proceder à une suppresion logique ou l\'archivage.',
     TVA_REQUIRED: "La tva est obligatoire.",
     MONTANT_MUST_BE_NUMBER: "Le montant du paiement doit être un entier.",
-    MONTANT_DEPASSE_RELIQUA: "Le montant de la vente depeasse le reliquat restant.",
+    MONTANT_DEPASSE_RELIQUA: "Le montant de la vente dépasse le reliquat restant.",
     PAIEMENT_NOT_EXIST: "Ce paiement n'existe pas ou son ID n'est pas valide.",
     NOT_EXIST_PAIEMENT: "Ce paiement n'existe pas ou son ID n'est pas valide.",
     NOT_REMOVABLE_PAIEMENT: "Vous ne pouvez pas supprimer ce paiement car il est lié à un article.",
